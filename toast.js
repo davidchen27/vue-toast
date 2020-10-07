bdcbfc5b1c10a825e2c0518603db855f3838beea
DavidChen <44220311+davidchen27@users.noreply.github.com>
2020-10-07 16:12:40 +0800

    解决IE兼容问题

--- toast.js
+++ toast.js
@@ -2,6 +2,14 @@ import Vue from 'vue'
 import Toast from "@/components/Module_Toast"
 
 const ToastConstructor = Vue.extend(Toast)
+// 判断是否为IE
+function isIE() {
+  return !!window.ActiveXobject || "ActiveXObject" in window;
+}
+function isIE11(){
+  return (/Trident\/7\./).test(navigator.userAgent);
+}
+
 function showToast(text, type = 'info', duration = 2000){
   const toastDom = new ToastConstructor({
     el: document.createElement('div'),
@@ -14,13 +22,19 @@ function showToast(text, type = 'info', duration = 2000){
   })
   document.getElementById('toast').appendChild(toastDom.$el)
   setTimeout(()=>{
-    toastDom.$el.remove()
+    if (isIE() || isIE11()) {
+      toastDom.$el.removeNode(true);
+    } else {
+      toastDom.$el.remove()
+    }
   }, duration)
 }
 function toastRegister(){
   const toastWrap = document.createElement('div')
+  const toastBoxStyle = document.createElement('style')
+  toastBoxStyle.innerText = "#toast{position: fixed;top: 60px;left: 50%;z-index: 998;transform: translateX(-50%);}"
+  document.body.appendChild(toastBoxStyle)
   toastWrap.id = 'toast'
-  toastWrap.style = "position: fixed;top: 60px;left: 50%;z-index: 998;transform: translateX(-50%);"
   document.body.appendChild(toastWrap)
   Vue.prototype.$toast = showToast
 }
